2324050b9052f8b3fdc88e36e2b7a96a05643f37
ozlem <ozlemakcicek83@gmail.com>
2023-05-28 16:49:08 +0200

    wrwer

---
+++ class-notes/My-Nots/react.jsx
@@ -0,0 +1,46 @@
+// //! Harici bir Apiden veri alirken projeler my-Random-User a bak;
+//   const [contacts, setContacts] = useState([]);
+  
+//   useEffect(() => {
+//     const getVeri = async () => {
+//       const res = await axios.get("https://randomuser.me/api/");
+//       // console.log(res.data.results);
+//       setContacts(res.data.results);
+//     };
+//     getVeri();
+//   }, []);
+//     console.log(contacts);
+
+    
+//   return (
+
+//     <div className="App ">
+//       {contacts.map((eleman) => {
+//         return (
+//           <div className="container mt-5" >
+//             <div className="ust d-flex ">
+//               <img className="pic" src={eleman.picture.large} alt="" />
+
+            
+
+
+
+//! Local deki Database den veri cekerken DE-!0-Appointment a bak:
+// import { data } from "../helpers/data";
+
+
+// const Home = () => {
+
+// const [doktorlar, setDoktorlar] = useState(doktorData);
+// return (
+//     <div style={{display:tikla? "block" : "flex"}}>
+//       <div>
+//         <header className="header">
+//           <h1>HOSPİTAL</h1>
+//           <div className="dr">
+//             {doktorlar.map((dr) => (
+//               <div key={dr.id}>....
+              
+
+
+          
