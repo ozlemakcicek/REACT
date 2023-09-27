5e16de7f9a3bcd3c117aa60f71c5df50e7a98ffd
ozlem <ozlemakcicek83@gmail.com>
2023-09-27 14:17:49 +0200

    cards grid

--- class-notes/DE-21-Material-UI/src/components/CardsGrid.jsx
+++ class-notes/DE-21-Material-UI/src/components/CardsGrid.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const CardsGrid = () => {
+  const [products, setProducts]=useState([])
+
+  useEffect(() => {
+
+  fetch("https://dummyjson.com/products")
+  .then((res)=>res.json())
+  .then((data)=>setProducts(data.products))
+    
+  }, [])
+ console.log(products); 
+
+
   return (
-    <div>CardsGrid</div>
+    <div>
+      
+    </div>
   )
 }
 export default CardsGrid
